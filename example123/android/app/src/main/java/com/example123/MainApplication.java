ff7437e17526d834c6c73542e4b2641582739b59
Bynn <865068275@qq.com>
2018-06-01 14:28:43 +0800

    fix
    fix some bug

--- example123/android/app/src/main/java/com/example123/MainApplication.java
+++ example123/android/app/src/main/java/com/example123/MainApplication.java
@@ -2,8 +2,8 @@ package com.example123;
 
 import android.app.Application;
 
+import com.enhancewebview.RNEnhanceWebviewPackage;
 import com.facebook.react.ReactApplication;
-import com.reactlibrary.RNEnhanceWebviewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNEnhanceWebviewPackage()
+              new RNEnhanceWebviewPackage()
       );
     }
 
